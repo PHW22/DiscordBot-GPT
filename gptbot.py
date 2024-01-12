84a12ef852c2935b9a2598035ebbcee4f5d4ad3b
PHW22 <pinhanwu22@gmail.com>
2024-01-12 20:47:12 +0800

    註解更新

--- gptbot.py
+++ gptbot.py
@@ -1,41 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  8 00:36:41 2024
-
-@author: d1a1n
-"""
-
-# 導入Discord.py模組
+# 導入Discord.py模組和openai模組
 import discord
 from openai import OpenAI
 
 
-# client是跟discord連接，intents是要求機器人的權限
+# 預設機器人的權限，機器人可以觀看聊天內容
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents = intents)
 
+#輸入OpenAI API金鑰
 cli = OpenAI(api_key = 'GPT_API_金鑰')
 
 # 調用event函式庫
 @client.event
-# 當機器人完成啟動
+# 當機器人完成啟動時告知"chatGPT 已啟動"
 async def on_ready():
     print("chatGPT 已啟動")
 
-
 @client.event
-# 當頻道有新訊息
+# 偵測到Discord聊天頻道有新訊息
 async def on_message(message):
     # 排除機器人本身的訊息，避免無限循環
     if message.author == client.user:
         return
-
+    # message.content為Discord內的新訊息，為了與聊天內容區隔開頭需輸入/gpt
     if message.content[0:4] == "/gpt":
         response = cli.chat.completions.create(      
-            model      = "gpt-3.5-turbo",
-            messages   = [{"role": "user", "content": message.content}],
-            max_tokens = 1500)
+            model      = "gpt-3.5-turbo",                                 #使用模型                                      
+            messages   = [{"role": "user", "content": message.content}],  #向GPT詢問內容
+            max_tokens = 1500)                                            #GPT回答token限制
         if response.choices:
             await message.channel.send(response.choices[0].message.content)
         else:
@@ -43,7 +36,5 @@ async def on_message(message):
     else:
         return    
 
-
-    
-    
+#輸入Discord API金鑰    
 client.run("Discord_API_金鑰")
